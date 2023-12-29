ef009197b06daf72ca6c40ce460b7c13b1e54b85
jinhomun <answlsgh95@gmail.com>
2023-12-29 09:42:55 +0900

    dd2

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-// `fallback_image_url` 모듈과 파일 경로를 올바르게 지정해야 합니다.
-// import { fallback_image_url } from './path/to/fallback_image_url';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,12 +12,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'fallback_image_url': './path/to/fallback_image_url',
-    }
+    },
   },
   build: {
     rollupOptions: {
-      external: ['fallback_image_url'],
+      // "fallback_image_url"을 external에서 제거합니다.
+      external: [],
     },
   },
 });
